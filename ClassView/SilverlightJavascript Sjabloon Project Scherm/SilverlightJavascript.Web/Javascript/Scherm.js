a15e81736bfaaec296db94b84b195371127b0416
Shiouen <olivier.va@outlook.com>
2015-08-23 22:43:31 +0200

    divided seperate projects into their folders. Both projects run

---
+++ ClassView/SilverlightJavascript Sjabloon Project Scherm/SilverlightJavascript.Web/Javascript/Scherm.js
@@ -0,0 +1,20 @@
+﻿// Global variables.
+var xamlPage = null;
+
+var courses = null;
+
+var students = null;
+var currentStudent = null;
+
+var meansStdDevsExposed = false;
+var minMaxFocused = false;
+
+var highestScoreBorder = null;
+var highestScoreText = null;
+var lowestScoreBorder = null;
+var lowestScoreText = null;
+
+// Constructor.
+function onLoaded() {
+    xamlPage = document.getElementById('ClassViewPage');
+}
