24536536f1a659a777230527ca02b4ff4611c885
Olivier Van Aken <olivier.va@outlook.com>
2015-08-24 04:12:12 +0200

    Tested ClassView and StudentView on other device. Competence C# project has been initialized (can view xaml).

---
+++ CompetenceView/SilverlightCSharp Sjabloon CompetenceView/SilverlightCSharp/Views/CompetenceView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace SilverlightCSharp.Views {
+    public partial class CompetenceView : Page {
+        public CompetenceView() {
+            InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+       
+    }
+}
